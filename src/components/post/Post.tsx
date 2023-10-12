6634b0bdb5c18da875e862fe00b016175bcd08ef
Aysia Brown <aysiaebrown@gmail.com>
2023-10-12 14:23:54 -0700

    Setting up mockdata

---
+++ src/components/post/Post.tsx
@@ -0,0 +1,16 @@
+import { Avatar } from "@mui/material"
+
+export function Post() {
+    return (
+        <div>
+            <div>
+            <Avatar>HV</Avatar>
+            <div>
+                <p>Hera Varela</p>
+                <p>@StardustHera</p>
+            </div>
+            </div>
+
+        </div>
+    )
+}
