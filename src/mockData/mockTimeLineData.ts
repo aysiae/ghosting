79cd67a39828e0916134b16bcbe894020c828bda
Aysia Brown <aysiaebrown@gmail.com>
2023-10-16 14:02:36 -0700

    Expanding mock data, adding icons to posts

--- src/mockData/mockTimeLineData.ts
+++ src/mockData/mockTimeLineData.ts
@@ -1,6 +1,6 @@
 import { PostType } from "../types/postTypes";
 
-const heraTweets : PostType = {
+const hera_1 : PostType = {
     name: 'Hera Varela',
     username: 'StardustHera',
     avatar: 'HV',
@@ -8,7 +8,31 @@ const heraTweets : PostType = {
     date: 'October 12, 2023',
 }
 
-const valkyrieTweet : PostType = {
+const hera_2 : PostType = {
+    name: 'Hera Varela',
+    username: 'StardustHera',
+    avatar: 'HV',
+    body: 'Lauv is my favoriteeee',
+    date: 'October 12, 2023',
+}
+
+const dimitri_1 : PostType = {
+    name: 'Dimitri',
+    username: 'pinkgrimreaper',
+    avatar: 'D',
+    body: 'MOLLY IN MEXICO!!!',
+    date: 'October 14, 2023',
+}
+
+const dimitri_2 : PostType = {
+    name: 'Dimitri',
+    username: 'pinkgrimreaper',
+    avatar: 'D',
+    body: '1 like to win a date with Paulie',
+    date: 'October 14, 2023',
+}
+
+const valkyrie_1 : PostType = {
     name: 'Valkyrie',
     username: 'coquetteharpy',
     avatar: 'V',
@@ -16,4 +40,32 @@ const valkyrieTweet : PostType = {
     date: 'October 10, 2023',
 }
 
-export const mockTimelineData = [heraTweets, valkyrieTweet];
+
+const valkyrie_2 : PostType = {
+    name: 'Valkyrie',
+    username: 'coquetteharpy',
+    avatar: 'V',
+    body: 'CHOKIN ON BRAIN MATTER AND MAKEUP',
+    date: 'October 10, 2023',
+}
+
+const gemini_1 : PostType = {
+    name: 'Gem',
+    username: 'gemcala',
+    avatar: 'G',
+    body: 'Flash tattoos!',
+    date: 'October 10, 2023',
+}
+
+const gemini_2 : PostType = {
+    name: 'Gem',
+    username: 'gemcala',
+    avatar: 'G',
+    body: 'Uhhhuhh',
+    date: 'October 10, 2023',
+}
+
+
+
+
+export const mockTimelineData = [hera_1, valkyrie_1, valkyrie_2, hera_2, dimitri_1, gemini_1, dimitri_2, gemini_2];
