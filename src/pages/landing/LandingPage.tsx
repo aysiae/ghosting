4f05378af6104e97f2ea71c4a648b3ae9eeb903f
Aysia Brown <aysiaebrown@gmail.com>
2023-10-18 14:28:50 -0700

    FIXING IMPORT ERROR ARGGGG

--- src/pages/landing/LandingPage.tsx
+++ src/pages/landing/LandingPage.tsx
@@ -2,7 +2,7 @@ import { Button, ButtonProps } from "@mui/material";
 import { styled } from "@mui/material/styles";
 import { deepPurple } from "@mui/material/colors";
 import { useState } from "react";
-import { SignIn } from "../../components/signin/signIn";
+import { SignIn } from "../../components/signin/SignIn";
 
 const LandingPageButton = styled(Button)<ButtonProps>(({ theme }) => ({
   color: theme.palette.getContrastText(deepPurple[300]),
