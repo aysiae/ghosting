3f3b13a4cf573ac6b4d54712be55e9d873ee6ada
Aysia Brown <aysiaebrown@gmail.com>
2023-11-13 15:06:45 -0800

    adding remember  me checkbox and disabling buttons if fields are not filled out

--- src/components/authenticator/Authenticator.tsx
+++ src/components/authenticator/Authenticator.tsx
@@ -1,4 +1,4 @@
-import { FormControl, TextField, Button, Box, Typography} from "@mui/material";
+import { FormControl, TextField, Button, Box, Typography, Checkbox} from "@mui/material";
 import { useState } from "react";
 
 import { SignIn } from "../../utils/auth/signIn";
@@ -16,6 +16,7 @@ export function Authenticator({ signIn }: { signIn: boolean }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [isMatching, setIsMatching] = useState(true);
+  const [rememberMe, setRememberMe] = useState(false);
 
   const emailCapture = (e: any) => {
     setEmail(e.target.value);
@@ -81,10 +82,15 @@ export function Authenticator({ signIn }: { signIn: boolean }) {
                 onBlur={(e) => passwordCapture(e)}
                 placeholder="Password"
               />
+              <Box sx={{display: 'flex', flexDirection: 'row', mt:2}}>
+                <Checkbox sx={{width: '10px', height: '10px', marginTop: '0.25em'}} onChange={(e) => setRememberMe(!rememberMe)} /> 
+                <Typography sx={{ml: 1}}>Remember me?</Typography>
+              </Box>
               <Button
                 variant='contained'
                 onClick={(e) => launchSignIn()}
                 sx={{ marginTop: "2em" }}
+                disabled={email && password ? false: true}
               >
                 Sign In!
               </Button>
@@ -112,7 +118,7 @@ export function Authenticator({ signIn }: { signIn: boolean }) {
               <TextField
                 required
                 variant="standard"
-                type='passw'
+                type='password'
                 onBlur={(e) => passwordsMatch(e)}
                 placeholder="Retype Password"
               />
@@ -123,6 +129,7 @@ export function Authenticator({ signIn }: { signIn: boolean }) {
                 variant='contained'
                 onClick={(e) => launchSignUp()}
                 sx={{ marginTop: "2em" }}
+                disabled={email && password && isMatching ? false: true}
               >
                 Sign Up!
               </Button>
