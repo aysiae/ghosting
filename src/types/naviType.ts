0dfe5ce0fc335153183bf161acb81977f60daa7c
Aysia Brown <aysiaebrown@gmail.com>
2024-01-17 11:14:15 -0800

    saving type rename

--- src/types/naviType.ts
+++ src/types/naviType.ts
@@ -1,11 +1,12 @@
 import { RouteType } from "./routeType";
 
-export type NavBarType = {
-  home: RouteType;
-  explore: RouteType;
-  notifs: RouteType;
-  messages: RouteType;
-  profiles: RouteType;
-  account: RouteType;
-  activeProfile: RouteType;
-};
+export type NaviType = {
+    home: RouteType,
+    explore: RouteType,
+    notifs: RouteType,
+    messages: RouteType,
+    profiles: RouteType,
+    account: RouteType,
+    activeProfile: RouteType,
+
+}
